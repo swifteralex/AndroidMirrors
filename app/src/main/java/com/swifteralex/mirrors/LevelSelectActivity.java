45c88d6922cbd7309d574bb6aa3060c65af55216
swifteralex <swifteralexc@gmail.com>
2020-07-10 21:15:16 -0700

    Added rays and a back button to level select screen.

--- app/src/main/java/com/swifteralex/mirrors/LevelSelectActivity.java
+++ app/src/main/java/com/swifteralex/mirrors/LevelSelectActivity.java
@@ -1,9 +1,15 @@
 package com.swifteralex.mirrors;
 
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LevelSelectActivity extends AppCompatActivity {
@@ -15,5 +21,18 @@ public class LevelSelectActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         setContentView(R.layout.level_select);
+
+        RotateAnimation rotateAnimation = new RotateAnimation(0, 360f,
+                Animation.RELATIVE_TO_SELF, 0.5f,
+                Animation.RELATIVE_TO_SELF, 0.5f);
+        rotateAnimation.setInterpolator(new LinearInterpolator());
+        rotateAnimation.setDuration(50000);
+        rotateAnimation.setRepeatCount(Animation.INFINITE);
+        findViewById(R.id.imageView2).startAnimation(rotateAnimation);
+    }
+
+    public void backArrowClicked(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
